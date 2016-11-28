450ca78dd5a49109e7433140ecd7fe7954c85d37
LammerTeam <MrLammerM@Gmail.com>
2016-11-28 10:07:09 +0330

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,9 @@
+bot_token = "262465817:AAF-T-gYde6iiomiVFAqbScxfRcJfuUHpoc"
+send_api = "https://api.telegram.org/bot"..bot_token
+bot_version = "1.0"
+sudo_name = "Mahdy"
+sudo_id = 115442858
+admingp = -174235550
+sudo_num = "989114762219"
+sudo_user = "MrLammer"
+sudo_ch = "LammerTeam"
